ede2fbe791d509181c8fc58e4714c2937ba2e0c3
thanhduy26091995 <buivuthanhduy@gmail.com>
2017-06-08 10:38:36 +0700

    fix

--- app/src/main/java/com/hbbsolution/maid/api/ApiInterface.java
+++ app/src/main/java/com/hbbsolution/maid/api/ApiInterface.java
@@ -1,13 +1,10 @@
 package com.hbbsolution.maid.api;
 
-<<<<<<< Updated upstream
 
 import com.hbbsolution.maid.history.model.OwnerHistoryResponse;
 import com.hbbsolution.maid.history.model.WorkHistoryResponse;
 import com.hbbsolution.maid.workmanager.listworkmanager.model.workmanager.WorkManagerResponse;
-=======
 import com.hbbsolution.maid.model.task_around.TaskAroundResponse;
->>>>>>> Stashed changes
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -20,6 +17,7 @@ import retrofit2.http.Query;
 public interface ApiInterface {
     @GET("more/getTaskAround")
     Call<TaskAroundResponse> getAllTask(@Query("lat") Double lat, @Query("lng") Double lng, @Query("maxDistance") Integer maxDistance);
+
     @GET("maid/getAllTasks")
     Call<WorkManagerResponse> getInfo(@Query("process") String idProcess, @Query("sortByTaskTime") boolean isSortByTaskTime);
 
