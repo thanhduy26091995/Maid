6e2acb9ce6627982ee2878a139e3656da8b23821
tranthienphuc <tranthienphuc518@gmail.com>
2017-06-22 10:48:00 +0700

    Contact

--- app/src/main/java/com/hbbsolution/maid/api/ApiInterface.java
+++ app/src/main/java/com/hbbsolution/maid/api/ApiInterface.java
@@ -8,8 +8,9 @@ import com.hbbsolution.maid.model.geocodemap.GeoCodeMapResponse;
 import com.hbbsolution.maid.model.task.TaskResponse;
 import com.hbbsolution.maid.model.task_around.TaskAroundResponse;
 import com.hbbsolution.maid.more.duy_nguyen.model.ReportResponse;
-import com.hbbsolution.maid.more.phuc_tran.model.AboutResponse;
 import com.hbbsolution.maid.more.duy_nguyen.model.StatisticResponse;
+import com.hbbsolution.maid.more.phuc_tran.model.AboutResponse;
+import com.hbbsolution.maid.more.phuc_tran.model.ContactResponse;
 import com.hbbsolution.maid.more.viet_pham.model.signin_signup.BodyResponse;
 import com.hbbsolution.maid.workmanager.detailworkmanager.model.JobPendingResponse;
 import com.hbbsolution.maid.workmanager.listworkmanager.model.workmanager.WorkManagerResponse;
@@ -78,8 +79,11 @@ public interface ApiInterface {
     @GET("maid/getTaskOfOwner")
     Call<WorkHistoryResponse> getListWorkByMaid(@Query("owner") String idOwner,@Query("page") int page);
 
-    @GET("more/getTerm")
-    Call<AboutResponse> getAbout (@Query("id") String idTask);
+    @GET("more/getGV24HInfo")
+    Call<AboutResponse> getAbout(@Query("id") String idTask);
+
+    @GET("more/getContact")
+    Call<ContactResponse> getContact();
 
     @GET("maid/statistical")
     Call<StatisticResponse> getStatistic(@Query("startAt") String startAt, @Query("endAt") String endAt);
